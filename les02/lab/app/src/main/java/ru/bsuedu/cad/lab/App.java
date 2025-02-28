aacee2393b563a6d7deb08304989e575cb7f28d9
zaic-bsu <zaickun1990@gmail.com>
2025-02-28 19:06:01 +0300

    Лабораторная работа №1

--- les02/lab/app/src/main/java/ru/bsuedu/cad/lab/App.java
+++ les02/lab/app/src/main/java/ru/bsuedu/cad/lab/App.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
     public static void main(String[] args) throws NumberFormatException, IOException, ParseException {
         ApplicationContext context = new AnnotationConfigApplicationContext(MessageConfiguration.class);
-        Renderer mr = context.getBean("renderer", Renderer.class);
-        mr.render();
+        Renderer renderer = context.getBean("renderer", Renderer.class);
+        renderer.render();
      }
 }
