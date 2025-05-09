56ad4b969e62c4a66b1471ed00c537b3fc88eca4
Жуков Дмитрий <dimaroot18@mail.ru>
2025-05-09 22:52:28 +0300

    2lab

---
+++ les04/lab/app/src/main/java/ru/bsuedu/cad/lab/ApplicationContext.java
@@ -0,0 +1,10 @@
+package ru.bsuedu.cad.lab;
+
+public class ApplicationContext {
+
+    public Renderer getBean(String string, Class<Renderer> class1) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBean'");
+    }
+
+}
